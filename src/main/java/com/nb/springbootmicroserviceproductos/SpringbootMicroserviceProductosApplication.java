7f20c382a6f466b369f18d28e51a2eb9266fa66e
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-15 16:58:36 -0500

    Proyecto inicial con spring web, JPA, H2, dev tools

---
+++ src/main/java/com/nb/springbootmicroserviceproductos/SpringbootMicroserviceProductosApplication.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroserviceProductosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nb/springbootmicroserviceproductos/SpringbootMicroserviceProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceProductosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
