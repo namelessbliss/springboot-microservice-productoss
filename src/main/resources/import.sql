82b3dc04e7d910382b86c5e9f4210820895e31a8
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-16 21:14:40 -0500

    Implementando balanceo de carga de lado del cliente con Ribbon

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('TV', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('RADIO', 120, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('CAFETERA', 200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('HORNO', 500, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('GUITARRA', 850, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('EQUIPO', 1200, NOW());
